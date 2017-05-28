3d7af97af99a427d700a44dd2aa016abb8921020
Suyash Shekhar <suyashshekhar@gmail.com>
2017-05-28 21:44:24 +0800

    Wrap game inside Game Class

--- pong.js
+++ pong.js
@@ -40,54 +40,61 @@ class Ball extends Rectangle {
   }
 }
 
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+
+    //Constructing a ball which is an object of the Ball class (which in turn
+    //inherits from the rectangle class)
+    this.ball = new Ball;
+
+    //Defining the velocity of the ball by default.
+    this.ball.velocity.x = 100;
+    this.ball.velocity.y = 100;
+
+    let lastTime;
+    //This function calls requestAnimationFrame recursively to animate according to
+    //updated positions
+    const callback = (milliseconds) => {
+      if (lastTime) {
+        var secondsSinceLastRefresh = (milliseconds - lastTime) / 1000;
+        this.updatePosition(secondsSinceLastRefresh);
+      }
+      lastTime = milliseconds;
+      requestAnimationFrame(callback);
+    };
+    callback();
+  }
+
+  //This function allows updating the position of the ball with respect to time.
+  // Distance = Speed x Time
+  updatePosition(time) {
+    this.ball.position.x += this.ball.velocity.x * time;
+    this.ball.position.y += this.ball.velocity.y * time;
+
+    //Preventing ball form moving out of the canvas; i.e, simulating bouncing off
+    //the walls. If the ball touches the edge of the canvas, invert the velocity.
+    if (this.ball.left < 0 || this.ball.right > this.canvas.width) {
+      this.ball.velocity.x = - this.ball.velocity.x;
+    }
+
+    if (this.ball.top < 0 || this.ball.bottom > this.canvas.height) {
+      this.ball.velocity.y = - this.ball.velocity.y;
+    }
+
+    //Filling the entire width x height of the canvas with a black background
+    this.context.fillStyle = '#000';
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    //Drawing a white ball
+    this.context.fillStyle = '#fff';
+    this.context.fillRect(this.ball.position.x, this.ball.position.y, this.ball.size.x, this.ball.size.y);
+  }
+
+}
+
+
 //Getting the Canvas element with the id 'pong'
 const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
-
-//Constructing a ball which is an object of the Ball class (which in turn
-//inherits from the rectangle class)
-const ball = new Ball;
-
-//Defining the velocity of the ball by default.
-ball.velocity.x = 100;
-ball.velocity.y = 100;
-
-let lastTime;
-
-//This function calls requestAnimationFrame recursively to animate according to
-//updated positions
-function callback(milliseconds) {
-  if (lastTime) {
-    secondSinceLastRefresh = (milliseconds - lastTime) / 1000;
-    updatePosition(secondSinceLastRefresh);
-  }
-  lastTime = milliseconds;
-  requestAnimationFrame(callback);
-}
-
-//This function allows updating the position of the ball with respect to time.
-// Distance = Speed x Time
-function updatePosition(time) {
-  ball.position.x += ball.velocity.x * time;
-  ball.position.y += ball.velocity.y * time;
-
-  //Preventing ball form moving out of the canvas; i.e, simulating bouncing off
-  //the walls. If the ball touches the edge of the canvas, invert the velocity.
-  if (ball.left < 0 || ball.right > canvas.width) {
-    ball.velocity.x = - ball.velocity.x;
-  }
-
-  if (ball.top < 0 || ball.bottom > canvas.height) {
-    ball.velocity.y = - ball.velocity.y;
-  }
-
-  //Filling the entire width x height of the canvas with a black background
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  //Drawing a white ball
-  context.fillStyle = '#fff';
-  context.fillRect(ball.position.x, ball.position.y, ball.size.x, ball.size.y);
-}
-
-callback();
+const game = new Game(canvas);
